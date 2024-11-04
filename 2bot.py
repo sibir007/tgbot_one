73117ba0e9514805b18d6b05553a039c08403ee3
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2024-11-04 23:16:41 +0300

    Запустил бота с различными  функциями

--- 2bot.py
+++ 2bot.py
@@ -1,7 +1,16 @@
 import telethon
-from telethon import TelegramClient, events
-
+import asyncio
+from telethon import TelegramClient, events, Button
+from telethon import functions, types
+from telethon.events.common import EventCommon
+import re
+from telethon.tl.custom.message import Message
 from decouple import Config, RepositoryEnv
+
+import logging
+logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(format="[%(levelname) %(asctime)s] %(name)s: %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # задаём расположение .env файла
 DOTENV_FILE = './.env'
@@ -22,20 +31,214 @@ bot_token = dotenv_config('BOOT_TOKEN')
 api_id = dotenv_config('APP_API_ID')
 api_hash = dotenv_config('APP_API_HASH')
 
-bot = TelegramClient('bot', api_hash=api_hash, api_id=api_id)
-bot.start(bot_token=bot_token)
 
-@bot.on(events.NewMessage(pattern='/start'))
-async def send_welcome(event):
-    print(event.stringify())
+bot_commands = [
+                types.BotCommand(
+                    command='start',
+                    description='start bot conversation'
+                ),
+                types.BotCommand(
+                    command='help',
+                    description='returns a help message, like a short text about what your bot can do and a list of commands'
+                ),
+                types.BotCommand(
+                    command='settings',
+                    description="shows the bot's settings for this user and suggests commands to edit them."
+                ),
+                types.BotCommand(
+                    command='ping',
+                    description="ping-pong"
+                ),
+            ]
+
+# bc = types.BotCommand(
+#     command='quiz',
+#     description="simple quiz"
+# )
+# bc.command
+
+def get_set_bot_commands_request(bot_commands: list[types.BotCommand]) -> functions.bots.SetBotCommandsRequest:
+
+    return functions.bots.SetBotCommandsRequest(
+            scope=types.BotCommandScopeDefault(),
+            lang_code='en',
+            commands=bot_commands
+    )
+
+async def set_all_bot_commands(bot):
     
-    await event.reply('Lalala lalala lalalala lala')
-    raise events.StopPropagation
+    request = get_set_bot_commands_request(bot_commands) 
+    result = await bot(request)
+    return result
+    
+async def deleted_message(bot: TelegramClient, chat_id, message: str, sek_delay: float):
+    msg: Message = await bot.send_message(chat_id, message=message)
+    await asyncio.sleep(sek_delay)
+    await msg.delete()
 
-@bot.on(events.NewMessage)
-async def echo_all(event):
-    await event.reply(f'bot echo... {event.text}')
-    # event.s
+
+# def init_not_deleted_message_command(bot):
+#     not_deleted_mes = 'This is not deleted msg'
+
+#     bot_commands.append(
+#                 types.BotCommand(
+#                     command='not_deleted_message',
+#                     description="test create non deleted msg"
+#                 ))
+#     msgs = []
+#     bot.on(events.NewMessage(pattern='/not_deleted_message'))
+#     async def handler(event):
+        
+    
+
+def init_mes_edit_command(bot):
+    resp_mes = """
+    Edit me command response:
+    ```
+    git clone https://github.com/SharzyL/tg_searcher.git telethon_examples2/
+    ```\n
+    **This is bold**\n
+    __This is italic__
+    """
+    
+    msgs = []
+    
+    @bot.on(events.NewMessage(pattern='/edit_me'))
+    async def register_edit_me_messages(event):
+        msgs.append(event.id)
+        raise events.StopPropagation()
+    
+    @bot.on(events.MessageEdited(func=lambda m: m.id in msgs))
+    async def handle(event):
+        resp = await event.respond(resp_mes)
+        await asyncio.sleep(5)
+        msgs.remove(event.id)
+        await event.delete()
+        await resp.delete()
+        raise events.StopPropagation()
+
+    bot_commands.append(
+                types.BotCommand(
+                    command='edit_me',
+                    description="test events.MessageEdited"
+                ))
+    
+        
+# test inline button
+def init_quiz_command(bot):
+    # {
+    #     chat_id = [mes1.id, mes2.id, mes3.id, ...],
+    #     ...,
+    #     ...,
+    # }
+    quiz_context = {}
+    @bot.on(events.NewMessage(pattern='/quiz'))
+    async def simple_quiz(event: EventCommon):
+        
+        # если в quiz_context существует event.chat_id key
+        # значит предыдущий /quiz не закончен, в этом случае
+        # сначала нужно закончить его
+        if event.chat_id in quiz_context:
+            await deleted_message(
+                bot, 
+                event.chat_id, 
+                'закончите предыдущий quiz',
+                3)
+            await event.delete()
+            raise events.StopPropagation()
+              
+        msg: Message = await event.reply(
+            'Yes or no?', 
+            buttons=[
+                Button.inline('Yes!', b'quiz_yes'), 
+                Button.inline('Nope', b'quiz_no')
+            ]
+        )
+        
+        msg_list: list = [event.id, msg.id]
+        # quiz_context.get(event.chat_id, [])
+        # msg_list.extend()
+        quiz_context[event.chat_id] = msg_list
+        # await remove_bot_commands(bot)
+        # await set_bot_commands_from_quiz(bot)
+        raise events.StopPropagation()
+
+    @bot.on(events.CallbackQuery(data=re.compile(b'quiz_')))
+    async def quiz_check(event: EventCommon):
+        if event.data == b'quiz_yes':
+            msg = await event.answer('Correct answer!', alert=True)
+            await bot.delete_messages(event.chat_id, quiz_context[event.chat_id])
+            del quiz_context[event.chat_id]
+            # quiz_context[event.chat_id].append(msg.id)
+        elif event.data == b'quiz_no':
+            msg = await event.answer('Wrong answer!', alert=True)
+            # quiz_context[event.chat_id].append(msg.id)
+        else:
+            msg = await event.answer('Ничего не подходит!', alert=True)
+            # quiz_context[event.chat_id].append(msg.id)
+        raise events.StopPropagation()()
+    
+    bot_commands.append(
+                types.BotCommand(
+                    command='quiz',
+                    description="test Button.inline"
+                ))
+            
+    
 
 
-bot.run_until_disconnected()
+async def main():
+    
+
+    bot = TelegramClient('bot', api_hash=api_hash, api_id=api_id)
+
+    await bot.start(bot_token=bot_token)
+
+    init_quiz_command(bot)
+    
+    init_mes_edit_command(bot)
+
+    @bot.on(events.MessageEdited)
+    async def message_edited_handler(event):
+        print('Message', event.id, 'changed at', event.date)
+
+    # @bot.on(events.UserUpdate)
+    # async def user_update_handler(event):
+    #     print(event.stringify())
+
+    @bot.on(events.NewMessage(pattern='/start'))
+    async def send_welcome(event: EventCommon):
+        await event.reply('Lalala lalala lalalala lala')
+        raise events.StopPropagation()
+
+
+    # @bot.on(events.NewMessage)
+    async def echo_all(event):
+        await event.reply(f'bot echo... {event.text}')
+        # event.s
+        raise events.StopPropagation()
+    
+    # @bot.on(events.NewMessage(outgoing=True, pattern='!ping'))
+    @bot.on(events.NewMessage(pattern='/ping'))
+    async def pong(event: EventCommon):
+        user = await event.get_chat()
+        logger.info(f'user {user}')
+        mes = await event.reply('!pong')
+        await asyncio.sleep(5)
+        await bot.delete_messages(event.chat_id, [event.id, mes.id])
+        raise events.StopPropagation()
+
+    await set_all_bot_commands(bot)
+
+    try:
+
+        await bot.run_until_disconnected()
+    finally:
+        await bot.disconnect()
+        
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
